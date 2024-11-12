25a900b7cfb6b2f82550b4d3a57ed7e0995a0ed4
Miran <miranabdelaziz@yahoo.com>
2024-11-12 00:25:06 -0500

    - installed reacter- dom-router - added Home, Login, Register screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import Register  from './screens/Register';
 
 
-function App() {
+const App = () => {
   return (
     <div >
       <h1>Hello world from react</h1>
---
+++ client/src/screens/Register.js
@@ -0,0 +1,9 @@
+const Register = () => {
+    return (
+      <div >
+        <h1>Hello world from react</h1>
+      </div>
+    );
+  }
+  
+  export default Register;
