7b464d71743f8a3831f12936a2e8fddb308e06b0
Miran <miranabdelaziz@yahoo.com>
2024-11-16 23:54:04 -0500

    - added routes for Home, Login and Register screens -Implemented authentication context with token refresh handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import Register  from './screens/Register';
@@ -7,9 +7,13 @@ import Register  from './screens/Register';
 
 const App = () => {
   return (
-    <div >
-      <h1>Hello world from react</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+    </Routes>    
+    </BrowserRouter>
   );
 }
 
